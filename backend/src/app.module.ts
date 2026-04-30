2ba081850fdfb9625af2e4a40388a88e39433f23
Aslam <aslam812943@gmai.com>
2026-04-30 22:32:57 +0530

    fix: database connection and port for render deployment

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UrlModule } from './modules/url/url.module';
 
@@ -11,7 +11,13 @@ import { UrlModule } from './modules/url/url.module';
     UserModule,
     AuthModule,
     UrlModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/url-shortener')
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
   ],
   // controllers: [],
   // providers: [],
